a5b7a7a19f0b2492f76def894f9e31209d1ef2ac
Thomas Page <thomas.page@gensys.com>
2023-10-01 16:22:33 -0400

    Updates

---
+++ precache-manifest.0ffd4a8b9351f18f490ffb7ccec16db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbc87d0f0e74b1f1f43e",
+    "url": "/karen/static/css/main.0f13f603.chunk.css"
+  },
+  {
+    "revision": "cbc87d0f0e74b1f1f43e",
+    "url": "/karen/static/js/main.88fae3c1.chunk.js"
+  },
+  {
+    "revision": "82dbe0bc98f3c6a9b7c4",
+    "url": "/karen/static/js/runtime~main.4ebcc9c1.js"
+  },
+  {
+    "revision": "8ec1c2da4b12f198543a",
+    "url": "/karen/static/js/2.ccd92a99.chunk.js"
+  },
+  {
+    "revision": "e38d937fb4fc94d89d9215fba7e5cefb",
+    "url": "/karen/index.html"
+  }
+];
