1775db6c1398d9763793272564dfc5b358458673
Thomas Page <thomas.page@gensys.com>
2023-10-01 16:08:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import AudioReactRecorder, { RecordState } from './Recorder'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      recordState: null,
+      canvasWidth: 500,
+      canvasHeight: 300,
+    }
+  }
+
+  componentDidMount = () => {
+
+    this.setState({
+      recordState: RecordState.START,
+      canvasWidth: window.innerWidth,
+      canvasHeight: window.innerHeight,
+    })
+
+  }
+
+  start = () => {
+    this.setState({
+      recordState: RecordState.START
+    })
+  }
+
+  stop = () => {
+    this.setState({
+      recordState: RecordState.STOP
+    })
+  }
+
+  //audioData contains blob and blobUrl
+  onStop = (audioData) => {
+    this.setState({
+      recordState: RecordState.STOP
+    })
+  }
+
   render() {
+    const { canvasHeight, canvasWidth, recordState } = this.state
+
+    const backgroundColor = "rgb(15,18,23)"
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        style={{
+          backgroundColor,
+        }}
+      >
+        <AudioReactRecorder
+          canvasHeight={canvasHeight}
+          canvasWidth={canvasWidth}
+          backgroundColor={backgroundColor}
+          foregroundColor="rgb(185,215,124)"
+          state={recordState}
+          onStop={this.onStop}
+        />
+
+        <button onClick={this.start}>Start</button>
+        <button onClick={this.stop}>Stop</button>
       </div>
-    );
+    )
   }
 }
 
