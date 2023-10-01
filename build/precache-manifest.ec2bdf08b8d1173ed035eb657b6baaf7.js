5b446db8a33c0426f4a18b5c4b4be2cbbb84c9b3
Thomas Page <thomas.page@gensys.com>
2023-10-01 16:11:39 -0400

    add build to git

---
+++ build/precache-manifest.ec2bdf08b8d1173ed035eb657b6baaf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d96e8272a53c86064ad",
+    "url": "/static/css/main.0f13f603.chunk.css"
+  },
+  {
+    "revision": "9d96e8272a53c86064ad",
+    "url": "/static/js/main.e02353d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ec1c2da4b12f198543a",
+    "url": "/static/js/2.ccd92a99.chunk.js"
+  },
+  {
+    "revision": "287facaf2a2ccad6bdbf796eff991dac",
+    "url": "/index.html"
+  }
+];
